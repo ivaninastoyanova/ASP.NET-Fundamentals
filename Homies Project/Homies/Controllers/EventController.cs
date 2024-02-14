49a29d6f195cf021ad5580c4c006634e2bf14375
Ivanina Stoyanova <ivoka674@abv.bg>
2024-02-14 13:28:34 +0200

    All events page added. If user is logged in, always redirect to All events page when trying to open the home page.

---
+++ Homies Project/Homies/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using Homies.Data;
+using Homies.Models.Event;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homies.Controllers
+{
+    [Authorize]
+    public class EventController : Controller
+    {
+        private readonly HomiesDbContext data;
+
+        public EventController(HomiesDbContext context)
+        {
+            data = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var eventsToDisplay = await data
+                .Events
+                .Select(e => new EventInfoViewModel()
+                {
+                   Name = e.Name,
+                   Id = e.Id,
+                   Start = e.Start.ToString(DataConstants.DateFormat),
+                   Type = e.Type.Name,
+                   Organiser = e.Organiser.UserName
+                })
+                .ToListAsync();
+
+            return View(eventsToDisplay);
+        }
+    }
+}
