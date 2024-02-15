64bd4ad39fbea2ab997c115336525af51e7d8038
Ivanina Stoyanova <ivoka674@abv.bg>
2024-02-15 13:19:14 +0200

    "My books" page added

--- Library Project/Library/Controllers/BookController.cs
+++ Library Project/Library/Controllers/BookController.cs
@@ -1,10 +1,13 @@
 ﻿using Library.Data;
 using Library.Models.Book;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Library.Controllers
 {
+    [Authorize]
     public class BookController : Controller
     {
 
@@ -34,5 +37,34 @@ namespace Library.Controllers
 
             return View(booksToDisplay);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Mine()
+        {
+            string currentUserId = GetUserId();
+
+            var mineBooks = await data
+                .IdentityUsersBooks
+                .Where(ub => ub.CollectorId == currentUserId)
+                .Select(ub => new MineBooksViewModel()
+                {
+                    Id = ub.BookId,
+                    Description = ub.Book.Description,
+                    Title = ub.Book.Title,
+                    Author = ub.Book.Author,
+                    ImageUrl= ub.Book.ImageUrl,
+                    Category = ub.Book.Category.Name
+                })
+                .ToListAsync();
+
+            return View(mineBooks);
+        }
+
+
+        private string GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        }
+
     }
 }
---
+++ Library Project/Library/Models/Book/MineBooksViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.Models.Book
+{
+    public class MineBooksViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
