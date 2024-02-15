634ea2e46230943d36e523ea2260216482c5640c
Ivanina Stoyanova <ivoka674@abv.bg>
2024-02-15 13:07:03 +0200

    "All books" page added

---
+++ Library Project/Library/Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Library.Data;
+using Library.Models.Book;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+
+        private readonly LibraryDbContext data;
+
+        public BookController(LibraryDbContext context)
+        {
+            data = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var booksToDisplay = await data
+                .Books
+                .Select(b => new BookInfoViewModel()
+                {
+                    Id= b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Rating = b.Rating,
+                    ImageUrl = b.ImageUrl,
+                    Category = b.Category.Name
+                })
+                .ToListAsync();
+
+            return View(booksToDisplay);
+        }
+    }
+}
---
+++ Library Project/Library/Models/Book/BookInfoViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Library.Models.Book
+{
+    public class BookInfoViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Author { get; set; } = string.Empty;
+
+        public decimal Rating { get; set; }
+
+        public string ImageUrl { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
