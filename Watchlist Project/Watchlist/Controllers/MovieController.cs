543dac0bf842362747f8b1799a9a6fc758f9d1f0
Ivanina Stoyanova <ivoka674@abv.bg>
2024-02-16 17:46:41 +0200

    Created a page where users can see their watched movies. Possibility to add or remove movies to their watched collection.

--- Watchlist Project/Watchlist/Controllers/MovieController.cs
+++ Watchlist Project/Watchlist/Controllers/MovieController.cs
@@ -38,5 +38,95 @@ namespace Watchlist.Controllers
 
             return View(moviesToDisplay);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Watched()
+        {
+            string currentUserId = GetUserId();
+
+            var userMovies = await data
+                .UsersMovies
+                .Where(um => um.UserId == currentUserId)
+                .Select(um => new MovieInfoViewModel()
+                {
+                    Id = um.MovieId,
+                    Title = um.Movie.Title,
+                    Director = um.Movie.Director,
+                    Rating = um.Movie.Rating,
+                    Genre = um.Movie.Genre.Name,
+                    ImageUrl = um.Movie.ImageUrl
+                })
+                .ToListAsync();
+
+            return View(userMovies);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCollection(int id)
+        {
+            var currentMovie = await data
+                .Movies
+                .FindAsync(id);
+
+            if (currentMovie == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+
+            var entryToAdd = new UserMovie()
+            {
+                MovieId = currentMovie.Id,
+                UserId = currentUserId,
+            };
+
+            if (await data.UsersMovies.ContainsAsync(entryToAdd))
+            {
+                return RedirectToAction("All", "Movie");
+            }
+
+            await data.UsersMovies.AddAsync(entryToAdd);
+           
+            await data.SaveChangesAsync();
+
+            return RedirectToAction("All", "Movie");
+        //https://localhost:7290/Movie/AddToCollection?movieId=2
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromCollection(int id)
+        {
+            var movieId = id;
+            var movieToRemove = await data
+                .Movies
+                .FindAsync(movieId);
+
+            if (movieToRemove == null)
+            {
+                return BadRequest();
+            }
+
+            string currentUserId = GetUserId();
+
+            var entryToRemove = await data.UsersMovies
+                .FirstOrDefaultAsync(um => um.UserId == currentUserId && um.MovieId == movieId);
+
+            if (entryToRemove == null)
+            {
+                return BadRequest();
+            }
+
+            data.UsersMovies.Remove(entryToRemove);
+
+            await data.SaveChangesAsync();
+
+            return RedirectToAction("Watched", "Movie");
+        }
+
+        private string GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        }
     }
 }
