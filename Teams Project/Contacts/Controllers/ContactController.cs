9fd4727401f551b5df2b48bfa67b017aba843bd5
Ivanina Stoyanova <ivoka674@abv.bg>
2024-02-17 16:37:16 +0200

    "All contacts" page added

--- Teams Project/Contacts/Controllers/ContactController.cs
+++ Teams Project/Contacts/Controllers/ContactController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Contacts.Data;
+using Contacts.Models.Contact;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contacts.Controllers
 {
+    [Authorize]
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        private readonly ContactsDbContext data;
+
+        public ContactController(ContactsDbContext context)
         {
-            return View();
+            data = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var contactsToDisplay = await data
+                .Contacts
+                .Select(c => new AllContactViewModel()
+                {
+                    FirstName = c.FirstName,
+                    LastName = c.LastName,
+                    Email = c.Email,
+                    PhoneNumber = c.PhoneNumber,
+                    Address = c.Address,
+                    Website = c.Website
+                })
+                .ToListAsync();
+
+            return View(contactsToDisplay);
         }
     }
 }
---
+++ Teams Project/Contacts/Models/Contact/AllContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Contacts.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Contacts.Models.Contact
+{
+    public class AllContactViewModel
+    {
+       
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string Website { get; set; } = string.Empty;
+
+    }
+}
